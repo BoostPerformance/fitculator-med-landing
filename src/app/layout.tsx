1cf7e69d9b28e1c9d74f7bdf8b99df132df0ff73
sung-eunji <danceunji@gmail.com>
2024-12-10 16:36:43 +0100

    SEO set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Head from 'next/head';
 import Footer from './components/common/footer';
 import Header from './components/common/header';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'fitculator-med',
@@ -21,8 +22,6 @@ export default function RootLayout({
     <html lang="en" className="scrollbar-hide">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* 메타 설명과 키워드 */}
         <meta
           name="description"
           content="Fitculator로 BMI, 칼로리 소모량, 운동 계획을 계산하세요. 쉽고 빠르게 건강 목표를 달성하세요."
@@ -31,21 +30,19 @@ export default function RootLayout({
           name="keywords"
           content="건강 계산기, 체중 관리, BMI 계산기, 칼로리 계산, 운동 계획"
         />
-
-        {/* Google Analytics 코드 추가 */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
-        ></script>
-        <script>
-          {`
+      </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-XXXXXXX');
           `}
-        </script>
-      </Head>
+      </Script>
 
       <body className={`font-pretendard`}>
         <Header />
